5d293e505edfde61bf4f18662ce319714d3f00ab
LuK1337 <priv.luk@gmail.com>
2018-05-19 15:56:10 +0200

    sm7125-common: Switch to a map based BT name
    Co-authored-by: dianlujitao <dianlujitao@lineageos.org>

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -3,6 +3,7 @@
  *  Not a Contribution.
  *
  * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (C) 2018-2020 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +20,52 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-#define BTM_DEF_LOCAL_NAME   "Realme SDM720G"
+
+#include <stdint.h>
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+int property_get(const char *key, char *value, const char *default_value);
+#ifdef __cplusplus
+}
+#endif
+
+#include "osi/include/osi.h"
+
+typedef struct {
+    const char *product_device;
+    const char *product_prjname;
+    const char *product_model;
+} device_t;
+
+static const device_t devices[] = {
+    {"RMX2061L1", "19720", "Realme 6 Pro"},
+    {"RMX2170L1", "206B1", "Realme 7 Pro"},
+};
+
+static inline const char *BtmGetDefaultName()
+{
+    char product_device[92];
+    char product_prjname[92];
+    property_get("ro.product.device", product_device, "");
+    property_get("ro.boot.prjname", product_prjname, "");
+
+    for (unsigned int i = 0; i < ARRAY_SIZE(devices); i++) {
+        device_t device = devices[i];
+
+        if (strcmp(device.product_device, product_device) == 0 &&
+               (strcmp(device.product_prjname, product_prjname) == 0)) {
+           return device.product_model;
+       }
+    }
+
+    // Fallback to ro.product.model
+    return "";
+}
+
+#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    32
