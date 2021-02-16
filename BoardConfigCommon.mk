f5e3bf2986fb23c4133d479c8a852facbfe21465
TheMalachite <eliasgheeraert@gmail.com>
2021-02-17 00:35:15 +0100

    [DNM] sm7125-common: SELinux permissive

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -42,6 +42,7 @@ BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += androidboot.fstab_suffix=at.qcom
 BOARD_KERNEL_CMDLINE += androidboot.memcg=1
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200n8
 BOARD_KERNEL_CMDLINE += earlycon=msm_geni_serial,0xa88000
