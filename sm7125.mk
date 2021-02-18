11b5376e60a1cc8a7b2b41599029abc237a32ab1
TheMalachite <eliasgheeraert@gmail.com>
2021-02-18 20:58:36 +0100

    sm7125-common: Move inherit products targets list to common tree

--- sm7125.mk
+++ sm7125.mk
@@ -4,6 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/realme/sm7125-common/sm7125-common-vendor.mk)
 
