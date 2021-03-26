e443c5855fb7908ea4a9b1483f55965a151135ab
TheMalachite <eliasgheeraert@gmail.com>
2021-03-26 12:18:26 +0100

    Revert "sm7125-common: releasetools: Add firmware flashing"
    This reverts commit 4e1556763d483dd36c9885d4869a29d96ad990b3.

--- releasetools.py
+++ releasetools.py
@@ -21,71 +21,29 @@ def FullOTA_InstallBegin(info):
   data = info.input_zip.read("RADIO/dynamic-remove-oppo")
   common.ZipWriteStr(info.output_zip, "dynamic-remove-oppo", data)
   info.script.AppendExtra('update_dynamic_partitions(package_extract_file("dynamic-remove-oppo"));')
+  return
 
 def FullOTA_InstallEnd(info):
   OTA_InstallEnd(info, False)
+  return
 
 def IncrementalOTA_InstallEnd(info):
   OTA_InstallEnd(info, True)
+  return
 
-def AddImageOnly(info, basename, incremental, firmware):
+def AddImage(info, basename, dest, incremental):
+  name = basename
   if incremental:
     input_zip = info.source_zip
   else:
     input_zip = info.input_zip
-  if firmware:
-    data = input_zip.read("RADIO/" + basename)
-  else:
-    data = input_zip.read("IMAGES/" + basename)
-  common.ZipWriteStr(info.output_zip, basename, data)
-
-def AddImage(info, basename, dest, incremental):
-  AddImageOnly(info, basename, incremental, False)
-  info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
+  data = input_zip.read("IMAGES/" + basename)
+  common.ZipWriteStr(info.output_zip, name, data)
+  info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
 def OTA_InstallEnd(info, incremental):
   info.script.Print("Patching dtbo and vbmeta images...")
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo", incremental)
   AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta", incremental)
   AddImage(info, "vbmeta_system.img", "/dev/block/bootdevice/by-name/vbmeta_system", incremental)
-  Firmware_Images(info, incremental)
-
-def Firmware_Images(info, incremental):
-  img_map = {
-      'abl': ['abl'],
-      'aop': ['aop'],
-      'BTFM': ['bluetooth'],
-      'cdt_engineering': ['cdt_engineering'],
-      'cmnlib': ['cmnlib'],
-      'cmnlib64': ['cmnlib64'],
-      'devcfg': ['devcfg'],
-      'dpAP': ['apdp'],
-      'DRIVER': ['DRIVER'],
-      'dspso': ['dsp'],
-      'hyp': ['hyp'],
-      'imagefv': ['imagefv'],
-      'keymaster64': ['keymaster'],
-      'modem': ['modem'],
-      'oppo_sec': ['oppo_sec'],
-      'qupv3fw': ['qupfw'],
-      'splash': ['splash'],
-      'static_nvbk': ['oppostanvbk'],
-      'storsec': ['storsec'],
-      'tz': ['tz'],
-      'uefisecapp': ['uefisecapp'],
-      'xbl': ['xbl'],
-      'xbl_config': ['xbl_config']
-      }
-
-  fw_cmd = '\n(\n'
-  fw_cmd += 'ui_print("Flashing device specific firmware...");\n'
-
-  # Flash Device Specific Firmware
-  for img in img_map.keys():
-    AddImageOnly(info, '{}.img'.format(img), incremental, True)
-    for part in img_map[img]:
-      fw_cmd += 'package_extract_file("{}.img", "/dev/block/bootdevice/by-name/{}");\n'.format(img, part)
-  # END Flash Device Specific Firmware
-
-  fw_cmd += ')\n);'
-  info.script.AppendExtra(fw_cmd)
+  return
