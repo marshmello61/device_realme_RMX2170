2a730ff74bbea56a1498bab913d89925d9b0ec20
TheMalachite <eliasgheeraert@gmail.com>
2021-04-01 00:47:39 +0200

    !fixup: lights: Set correct module name for Lights HAL rc script

--- lights/Android.mk
+++ lights/Android.mk
@@ -14,7 +14,7 @@ LOCAL_SRC_FILES := \
     main.cpp
 
 LOCAL_REQUIRED_MODULES := \
-    android.hardware.lights-service.qti.rc
+    android.hardware.lights-qti.rc
 
 LOCAL_SHARED_LIBRARIES := \
     libbase \
@@ -27,13 +27,13 @@ include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := android.hardware.lights-service.qti.rc
+LOCAL_MODULE := android.hardware.lights-qti.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init
-LOCAL_MODULE_STEM := android.hardware.lights-service.qti.rc
+LOCAL_MODULE_STEM := android.hardware.lights-qti.rc
 
-LOCAL_SRC_FILES := android.hardware.lights-service.qti.rc
+LOCAL_SRC_FILES := android.hardware.lights-qti.rc
 
 include $(BUILD_PREBUILT)
