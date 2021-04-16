41d488df894e04b794806c741866cd076e12cd0e
TheMalachite <eliasgheeraert@gmail.com>
2021-04-16 17:54:29 +0200

    sm7125-common: bluetooth: Set correct prjname for Realme 6 Pro

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -41,7 +41,7 @@ typedef struct {
 } device_t;
 
 static const device_t devices[] = {
-    {"RMX2061L1", "19720", "Realme 6 Pro"},
+    {"RMX2061L1", "19721", "Realme 6 Pro"},
     {"RMX2170L1", "206B1", "Realme 7 Pro"},
 };
 
