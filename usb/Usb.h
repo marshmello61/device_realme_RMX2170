208db4ccb5bfa5b68b8c13a47117dfcafaa67f5a
TheMalachite <eliasgheeraert@gmail.com>
2021-04-21 17:04:59 +0200

    sm7125-common: usb: Rebrand USB basic 1.0 HAL to realme_sm7125

--- usb/Usb.h
+++ usb/Usb.h
@@ -26,7 +26,7 @@
 #undef LOG_TAG
 #endif
 
-#define LOG_TAG "android.hardware.usb@1.0-service-basic"
+#define LOG_TAG "android.hardware.usb@1.0-service-basic.realme_sm7125"
 #define UEVENT_MSG_LEN 2048
 
 namespace android {
